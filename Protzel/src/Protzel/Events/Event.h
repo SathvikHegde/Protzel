eeb8dd6ed0741071a3ed2cf7fdf99463bb59e2f6
PeePeeHadLegs <62826093+SathvikHegde@users.noreply.github.com>
2022-07-17 22:52:29 +0530

    Mouse events

--- Protzel/src/Protzel/Events/Event.h
+++ Protzel/src/Protzel/Events/Event.h
@@ -27,8 +27,8 @@ namespace Protzel
     };
 
 #define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::##type; }\
-                                virtual EventType GetEventType() const override { return GetStaticType; }\
-                                virtual const char* GetName() const override { return #type; }
+								virtual EventType GetEventType() const override { return GetStaticType(); }\
+								virtual const char* GetName() const override { return #type; }
 
 #define EVENT_CLASS_CATEGORY(category) virtual int GetCategoryFlags() const override { return category; }
 
@@ -75,4 +75,9 @@ namespace Protzel
     private:
         Event& m_Event;
     };
+
+    inline std::ostream& operator<<(std::ostream& os, const Event& e)
+    {
+        return os << e.ToString();
+    }
 }
